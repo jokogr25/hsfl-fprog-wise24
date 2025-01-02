46523ef9a02ba214f2e49de1acaf7bab85820d89
jokogr (Joscha Konstantin Groth) <jokogr@mailbox.org>
2025-01-02 07:24:36 +0100

    Implement Aufgabe 4

--- src/FP_Aufgaben_02/FP02.hs
+++ src/FP_Aufgaben_02/FP02.hs
@@ -22,6 +22,7 @@ potenz _ 0 = 1
 potenz x e = x * potenz x (e - 1)
 
 -- Aufgabe 3
+-- es wurde hier modulo verwendet, da es dann möglich ist, den Preis in jedem Schritt weiter zu erhöhen (220 mod 100 = 20)
 bonbon :: Int -> Int
 bonbon = bonbon' 10
   where
@@ -30,3 +31,19 @@ bonbon = bonbon' 10
       | geld >= preis = 1 + bonbon' (preis + 10) (geld - (preis `mod` 100))
       -- geld >= preis = 1 + bonbon' (if preis == 100 then 10 else preis + 10) (geld - preis)
       | otherwise = -1
+
+-- Aufgabe 4
+-- in der Aufgabe wird ein Hinweis gegeben, dass die Funktion mittels summe und laenge implementiert werden soll - diese müssen selbst implementiert werden
+mittelwert :: Int -> Int -> Double
+mittelwert _ 0 = 0
+mittelwert a b = fromIntegral (summe a b) / fromIntegral (laenge a b)
+
+summe :: Int -> Int -> Int
+summe a b
+  | a > b = 0
+  | otherwise = a + summe (a + 1) b
+
+laenge :: Int -> Int -> Int
+laenge a b
+  | a > b = 0
+  | otherwise = 1 + laenge (a + 1) b
